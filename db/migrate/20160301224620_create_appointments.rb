9ad47164093923d1c97396ef09b1669c7c710881
katikiharsha <katikiharsha@gmail.com>
2016-03-04 10:14:42 -0500

    Update 20160301224620_create_appointments.rb

--- db/migrate/20160301224620_create_appointments.rb
+++ db/migrate/20160301224620_create_appointments.rb
@@ -1,11 +1,4 @@
 class CreateAppointments < ActiveRecord::Migration
-  def change
-    create_table :appointments do |t|
-      t.integer :patient_id
-      t.integer :doctor_id
-      t.datetime :appointment_time
-
-      t.timestamps null: false
-    end
-  end
-end
+ belongs_to :doctor
+ belongs_to :patient
+ end
